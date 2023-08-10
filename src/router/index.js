09b40713ddc29d658b366c3ecdc974d198204bdf
kufre-abasi <kufreabasibassey3@gmail.com>
2023-08-10 17:34:11 +0100

    Docs: page renamed

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
-import LoginPage from '../views/Auth/Login.vue'
+import LoginPage from '../views/Auth/LoginPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
