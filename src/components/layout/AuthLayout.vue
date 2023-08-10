1a44d0c593bed066e7bac3897c099683043f7c2e
kufre-abasi <kufreabasibassey3@gmail.com>
2023-08-10 23:52:10 +0100

    Feat: login page

--- src/components/layout/AuthLayout.vue
+++ src/components/layout/AuthLayout.vue
@@ -1,25 +1,25 @@
 <template>
-    <div class="2xl:h-full h-screen">
-        <AuthHeader />
-        <div class="grid grid-cols-1 md:grid-cols-2 mt-20 gap-40 auth_container">
-            <div class="sticky md:flex items-center hidden  ">
-                <div class="relative md:block ">
-                    <div class="">
-                        <img class="w-[90%]" src="@/assets/image/Auth/AuthImage.png" alt="Auth">
-                    </div>
-                </div>
-            </div>
-            <div class="flex  justify-center items-center py-16">
-                <div class="w-[100%]">
-                    <slot></slot>
-                </div>
-            </div>
-
+  <div class="">
+    <AuthHeader />
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-40 auth_container">
+      <div class="sticky md:flex items-center hidden top-0">
+        <img
+          class="relative md:block"
+          src="@/assets/image/Auth/AuthImage.png"
+          alt="Auth"
+        />
+      </div>
+      <div
+        class="flex min-h-screen md:h-auto overflow-y-auto justify-center items-center lg:py-10"
+      >
+        <div class="w-[100%]">
+          <slot></slot>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import AuthHeader from '@/components/Header/AuthHeader.vue'
-
+import AuthHeader from "@/components/Header/AuthHeader.vue";
 </script>
