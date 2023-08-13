f00ece674d6bc934819e44e71c0434dad3cea5d6
kufre-abasi <kufreabasibassey3@gmail.com>
2023-08-13 11:03:10 +0100

    Docs: updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../views/Auth/LoginPage.vue'
-import Register from '../views/Auth/Register/RegisterPage.vue'
+import Register from '../views/Auth/RegisterPage.vue'
+import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'login',
-      component: LoginPage
+      redirect: "/dashboard",
     },
     {
       path: '/signup',
       name: 'signup',
       component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: HomeView
     },
   ]
 })
