b5fffd92d307781d13bfee0a37fcda30238af105
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-10 00:40:14 +0100

    Feat: green tick

---
+++ src/components/icons/greenInputTick.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="28"
+    height="28"
+    viewBox="0 0 28 28"
+    fill="none"
+  >
+    <path
+      d="M10.5 14.875L13.125 17.5L17.5 11.375M24.5 14C24.5 19.799 19.799 24.5 14 24.5C8.20101 24.5 3.5 19.799 3.5 14C3.5 8.20101 8.20101 3.5 14 3.5C19.799 3.5 24.5 8.20101 24.5 14Z"
+      stroke="#2F929C"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
