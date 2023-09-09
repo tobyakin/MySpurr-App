fd23bdb72056876f9141d8050e6b86a7981afb31
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-09 23:04:37 +0100

    Docs: portfolio modifed

--- src/components/ui/Form/Talent/PortfolioForm.vue
+++ src/components/ui/Form/Talent/PortfolioForm.vue
@@ -1,24 +1,44 @@
 <script setup>
-import { ref, onMounted, watch } from "vue";
+import { ref, onMounted, watch, defineAsyncComponent } from "vue";
 import { useOnboardingStore } from "@/stores/onBoarding";
 import { useStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
 import GlobalInput from "@/components/ui/GlobalInput.vue";
 import AttachFileIcon from "@/components/icons/attachFile.vue";
 import { useRouter } from "vue-router";
-// import Multiselect from "vue-multiselect";
+const SelectGroup = defineAsyncComponent(() =>
+  import("@/component/ui/Form/Input/SelectGroup.vue")
+);
 
 let store = useStore();
 console.log(store.getUser);
 let loading = ref(false);
 const router = useRouter();
-// const value = ref([{ name: "Javascript", code: "js" }]);
+const value = ref([]);
 
-// const options = ref([
-//   { name: "Vue.js", code: "vu" },
-//   { name: "Javascript", code: "js" },
-//   { name: "Open Source", code: "os" },
-// ]);
+const options = [
+  {
+    language: "Javascript",
+    libs: [
+      { name: "Vue.js", category: "Front-end" },
+      { name: "Adonis", category: "Backend" },
+    ],
+  },
+  {
+    language: "Ruby",
+    libs: [
+      { name: "Rails", category: "Backend" },
+      { name: "Sinatra", category: "Backend" },
+    ],
+  },
+  {
+    language: "Other",
+    libs: [
+      { name: "Laravel", category: "Backend" },
+      { name: "Phoenix", category: "Backend" },
+    ],
+  },
+];
 
 const OnboardingStore = useOnboardingStore();
 const { step } = storeToRefs(OnboardingStore);
@@ -31,7 +51,7 @@ const formState = {
   social_media_link: "",
 };
 const file = ref(null);
-const previewImage = ref(null);
+const uploadedFile = ref(null);
 
 onMounted(() => {
   checkVaildlity();
@@ -50,7 +70,7 @@ watch(
     checkVaildlity();
   },
   () => {
-    showImage();
+    uploadFile();
   }
 );
 
@@ -108,23 +128,26 @@ const submitForm = async (formData, payload) => {
   }
 };
 const uploadFile = () => {
-  file.value = previewImage.value.files[0];
-  showImage();
-};
-const showImage = async () => {
-  if (file.value) {
-    previewImage.value = URL.createObjectURL(file.value);
+  if (uploadedFile.value.files.length > 0) {
+    const selectedFileType = uploadedFile.value.files[0].type;
+    if (selectedFileType.startsWith("image/")) {
+      file.value = uploadedFile.value.files[0];
+    } else {
+      file.value = null;
+      alert("Please select an image file.");
+    }
   } else {
-    previewImage.value = null;
+    file.value = null;
   }
 };
+
 const prev = () => {
   emit("prev", step.value - 1);
 };
 </script>
 
 <template>
-  <section class="lg:w-[40%] animate__animated animate__fadeIn">
+  <form class="lg:w-[40%] animate__animated animate__fadeIn">
     <div class="w-auto">
       <h1 class="md:text-[36px] text-[#011B1F] font-EBGaramond500 text-2xl font-bold">
         Your portfolio
@@ -140,33 +163,36 @@ const prev = () => {
         class="flex-col flex gap-6 max-h-[65vh] overflow-y-auto py-12 hide-scrollbar overflow-hidden"
       >
         <div class="border-[0.737px] border-[#254035AB] rounded-[5.897px] p-4 py-1.5">
-          <label class="text-[#01272C] px-4 text-[12px] font-Satoshi400"
+          <label class="text-[#01272C] px-2 text-[12px] font-Satoshi400"
             >Rate/Compensation - Per hour</label
           >
           <GlobalInput
             v-model="formState.compensation"
             class="bg-transparent border-none"
             placeholder=""
+            required
           />
         </div>
         <div class="border-[0.737px] border-[#254035AB] rounded-[5.897px] p-4 py-1.5">
-          <label class="text-[#01272C] px-4 text-[12px] font-Satoshi400"
+          <label class="text-[#01272C] px-2 text-[12px] font-Satoshi400"
             >Portfolio Title</label
           >
           <GlobalInput
             v-model="formState.portfolio_title"
             class="bg-transparent border-none"
             placeholder=""
+            required
           />
         </div>
         <div class="border-[0.737px] border-[#254035AB] rounded-[5.897px] p-4 py-3.5">
-          <label class="text-[#01272C] px-4 text-[12px] font-Satoshi400"
+          <label class="text-[#01272C] px-2 text-[12px] font-Satoshi400"
             >Portfolio description</label
           >
           <textarea
             v-model="formState.portfolio_description"
             rows="4"
             class="bg-transparent opacity-[0.8029] w-full outline-none border-0 p-4 py-1.5"
+            required
           />
         </div>
 
@@ -174,29 +200,23 @@ const prev = () => {
           <input
             id="preview_Image"
             @change="uploadFile"
-            ref="previewImage"
+            ref="uploadedFile"
             type="file"
             hidden
+            accept="image/*"
+            required
           />
           <label
             for="preview_Image"
             class="cursor-pointer w-full justify-between flex text-[#01272C] px-4 text-[12px] font-Satoshi400"
-            >Upload photos <AttachFileIcon /></label
-          ><img :src="previewImage" alt="" />
+          >
+            <span v-if="!file">Upload photos</span
+            ><span v-if="file">{{ file.name }}</span> <AttachFileIcon
+          /></label>
         </div>
-        <!-- <Multiselect
-          v-model="value"
-          tag-placeholder="Add this as new tag"
-          placeholder="Search or add a tag"
-          label="name"
-          track-by="code"
-          :options="options"
-          :multiple="true"
-          :taggable="true"
-        ></Multiselect>
- -->
-        <div class="border-[0.737px] border-[#254035AB] rounded-[5.897px] p-4 py-1.5">
-          <label class="text-[#01272C] px-4 text-[12px] font-Satoshi400"
+
+        <!-- <div class="border-[0.737px] border-[#254035AB] rounded-[5.897px] p-4 py-1.5">
+          <label class="text-[#01272C] px-2 text-[12px] font-Satoshi400"
             >Please select social media link</label
           >
           <GlobalInput
@@ -204,7 +224,7 @@ const prev = () => {
             class="bg-transparent border-none"
             placeholder=""
           />
-        </div>
+        </div> -->
       </div>
       <!-- <div class="my-6">
         <button
@@ -224,12 +244,23 @@ const prev = () => {
       </button>
 
       <button
-        type="primary"
+        type="submit"
         @click="onFinish"
         class="bg-[#43D0DF] font-Satoshi500 text-white text-[14px] uppercase leading-[11.593px] rounded-full p-5 w-full"
       >
         Complete Profile
       </button>
     </div>
-  </section>
+  </form>
 </template>
+<style scoped>
+.multiselect__tag {
+  @apply !bg-brand font-Satoshi400;
+}
+.multiselect__tags-wrap {
+  @apply w-full;
+}
+.multiselect__tags {
+  @apply border-[0.737px] border-[#254035AB] rounded-[5.897px] p-4 py-3;
+}
+</style>
