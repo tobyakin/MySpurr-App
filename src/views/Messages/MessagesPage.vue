8d89ec921147783329903e1e7fddb2acab480fde
kufre-abasi <kufreabasibassey3@gmail.com>
2023-10-13 15:50:34 +0100

    Docs: updated

--- src/views/Messages/MessagesPage.vue
+++ src/views/Messages/MessagesPage.vue
@@ -6,9 +6,17 @@ import DashboardLayout from "@/components/layout/dashboardLayout.vue";
 <template>
   <DashboardLayout>
     <div
-      class="container flex flex-col lg:gap-[79.735px] gap-[40px] p-0 lg:p-6 lg:py-10 py-6 mb-10"
+      class="container flex flex-col lg:gap-[59px] gap-[34px] p-0 lg:p-6 lg:py-10 py-6 mb-10"
     >
-      messages
+      <h4 class="text-[#244034] font-EBGaramond500 capitalize text-[27px]">messages</h4>
+      <div class="flex flex-row gap-[34.16px] w-full max-h-[75vh] overflow-hidden">
+        <div
+          class="flex bg-white border-[0.449px] w-[45%] min-h-[75vh] border-[#254035AB] px-[38.15px] py-[20.72px]"
+        ></div>
+        <div
+          class="flex bg-white border-[0.449px] w-full border-[#254035AB] px-[38.15px] py-[20.72px]"
+        ></div>
+      </div>
     </div>
   </DashboardLayout>
 </template>
