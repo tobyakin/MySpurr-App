ee13eb6da3b1ed634e92ee17aeacee3ade549795
kufre-abasi <kufreabasibassey3@gmail.com>
2023-10-17 14:25:41 +0100

    Docs: padding

--- src/components/layout/dashboardLayout.vue
+++ src/components/layout/dashboardLayout.vue
@@ -81,7 +81,7 @@ const redirectToWallet = () => {
       <div class="hidden lg:block flex-shrink-0 md:w-[256px]">
         <BaseSidebar />
       </div>
-      <div class="flex-grow">
+      <div class="flex-grow pb-20">
         <!-- top-menu flex items-center justify-between py-3 px-4 lg:px-0 -->
         <!-- flex items-center gap-[3rem] justify-between px-4 min-[370px]:px-6 sticky top-0 pt-4 min-[370px]:pt-5 bg-white z-50  -->
         <nav
