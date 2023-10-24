1567871e2e2a713fe5377b344f1409b06179ac51
kufre-abasi <kufreabasibassey3@gmail.com>
2023-10-24 17:19:05 +0100

    feat: overview  end point used

--- src/components/ui/ProfileEdit/Forms/OverviewPage.vue
+++ src/components/ui/ProfileEdit/Forms/OverviewPage.vue
@@ -3,11 +3,25 @@ import { ref, onMounted } from "vue";
 import { useUserProfile } from "@/stores/profile";
 
 const userProfile = useUserProfile();
-const formState = ref({
-  overview: "",
-});
+import { storeToRefs } from "pinia";
+
+const { overview } = storeToRefs(userProfile);
+let loading = ref(false);
+
 const prefillDetails = () => {
-  formState.value.overview = userProfile.user?.data?.overview || "";
+  overview.value = userProfile.user?.data?.overview || "";
+};
+const onFinish = async () => {
+  loading.value = true;
+  try {
+    const res = await userProfile.handleUpdateOverview();
+    userProfile.userProfile();
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
 };
 
 onMounted(async () => {
@@ -20,7 +34,7 @@ onMounted(async () => {
     <div class="flex flex-row justify-between gap-[21px]">
       <div class="w-full flex flex-col gap-2 justify-between">
         <textarea
-          v-model="formState.overview"
+          v-model="overview"
           rows="8"
           class="bg-transparent font-Satoshi400 w-full outline-none text-sm border-[0.737px] border-[#254035AB] rounded-[5.897px] p-2 py-1.5"
         />
@@ -28,6 +42,7 @@ onMounted(async () => {
     </div>
     <div class="w-full flex justify-center mt-8">
       <button
+        @click="onFinish"
         class="btn-brand !border-none !w-[30%] mx-auto !py-3 lg:!px-10 !px-5 !text-[#FFFFFF] text-center !bg-[#2F929C]"
       >
         Save
