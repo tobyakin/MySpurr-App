c9d991a9998ec797ab81e5974250f4b228681fac
kufre-abasi <kufreabasibassey3@gmail.com>
2023-11-15 16:03:00 +0100

    feat: image added to dropdown

--- src/components/ui/DropDown.vue
+++ src/components/ui/DropDown.vue
@@ -13,7 +13,15 @@
         role="button"
         class="h-10 w-10 flex justify-center items-center rounded-full bg-brand"
       >
+        <img
+          v-if="userDetails?.image"
+          class="w-10 h-10 rounded-full"
+          :src="userDetails?.image"
+          alt=""
+        />
+
         <svg
+          v-else
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
