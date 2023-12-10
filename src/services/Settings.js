93b5d17759f8ff420c1fdf85ce4c9cd052460f62
kufre-abasi <kufreabasibassey3@gmail.com>
2023-12-10 14:20:45 +0100

    feat:settings

---
+++ src/services/Settings.js
@@ -0,0 +1,19 @@
+import axios from '../axios'
+import { catchAxiosError, catchAxiosSuccess } from './Response'
+import { getToken } from './Auth'
+
+export const updateUserSettings = async (talent_id,payload) => {
+  const token = await getToken()
+  try {
+    let res = await axios.patch(`accounts/${talent_id}`,payload, {
+      headers: {
+        Authorization: 'Bearer ' + token
+      }
+    })
+    catchAxiosSuccess(res)
+    return res.data
+  } catch (error) {
+    catchAxiosError(error)
+    throw error
+  }
+}
---
+++ src/stores/settings.js
@@ -0,0 +1,59 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { updateUserSettings } from '@/services/Setting'
+
+export const useUserSettingsStore = defineStore('settings', () => {
+
+  const settingsData = ref({
+    talent_id: null,
+    first_name: "",
+    last_name: "",
+    email: "",
+    location: "",
+    currency: "",
+    application_lin: "",
+    country_code: "",
+    phone_number: "",
+    billing_address: {
+        country: "",
+        state: "",
+        address_1: "",
+        address_2: "",
+        city: "",
+        zip_code: ""
+    },
+    language: []
+})
+  const userSettings = async () => {
+        let payload = {
+          first_name: settingsData.value.first_name,
+          last_name: settingsData.value.last_name,
+          email: settingsData.value.email,
+          location: settingsData.value.location,
+          currency: settingsData.value.currency,
+          application_lin: settingsData.value.application_lin,
+          country_code: settingsData.value.country_code,
+          phone_number: settingsData.value.phone_number,
+          billing_address: {
+            country: settingsData.value.billing_address.country,
+            state: settingsData.value.billing_address.state,
+            address_1: settingsData.value.billing_address.address_1,
+            address_2: settingsData.value.billing_address.address_2,
+            city: settingsData.value.billing_address.city,
+            zip_code: settingsData.value.billing_address.zip_code
+          },
+          language: settingsData.value.billing_address.language
+        }
+    try {
+      let res = await updateUserSettings(settingsData.value.talent_id, payload)
+      return res
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return {
+    userSettings,
+    settingsData
+  }
+})
