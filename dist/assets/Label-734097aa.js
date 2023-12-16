895f40c3bd437e2f02cd168dc290a134dc3f4afe
kufre-abasi <kufreabasibassey3@gmail.com>
2023-12-16 16:59:14 +0100

    build

--- dist/assets/Label-734097aa.js
+++
@@ -1,1 +0,0 @@
-import{f as s,o as t,a,r,m as o}from"./index-4a3c24bb.js";const n={};function c(e,l){return t(),a("label",o(e.$attrs,{class:"font-Satoshi500 text-[15.606px]"}),[r(e.$slots,"default")],16)}const m=s(n,[["render",c]]);export{m as default};
