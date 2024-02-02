3cbdd99e4fc13bc2b10e662414989c310fbf21bd
kufre-abasi <kufreabasibassey3@gmail.com>
2024-02-02 23:03:05 +0100

    feat: map

--- src/components/ui/Map/Map.vue
+++ src/components/ui/Map/Map.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <l-map :useGlobalLeaflet="false" :zoom="zoom">
-      <l-geo-json />
-      <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        :attribution="attribution"
-      ></l-tile-layer>
-      <!-- :geojson="geojson" :options="geojsonOptions" -->
-      <l-marker :lat-lng="markerLatLng">
-        <l-popup>Marker Popup</l-popup>
-      </l-marker>
+    <l-map :zoom="zoom" :center="center" style="height: 500px; width: 100%">
+      <l-tile-layer :url="url" :attribution="attribution" />
+      <l-geo-json
+        v-if="show"
+        :geojson="geojson"
+        :options="options"
+        :options-style="styleFunction"
+      />
+      <l-marker :lat-lng="marker" />
     </l-map>
   </div>
 </template>
@@ -28,19 +27,77 @@ import {
   LPolygon,
   LGeoJson,
 } from "@vue-leaflet/vue-leaflet";
+import { latLng } from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 
 export default {
+  name: "sampl-map",
+  components: {
+    LMap,
+    LTileLayer,
+    LGeoJson,
+    LMarker,
+  },
+  props: { lat: String, lng: String },
+
   data() {
     return {
-      zoom: 15,
-      center: [51.505, -0.159],
-      markerLatLng: [51.504, -0.159],
-      iconWidth: 25,
-      iconHeight: 40,
-      attribution: "© OpenStreetMap contributors",
+      loading: false,
+      show: true,
+      enableTooltip: true,
+      zoom: 6,
+      center: [48, -1.219482],
+      geojson: null,
+      fillColor: "#e4ce7f",
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: latLng(this.lat, this.lng),
     };
+  },
+  computed: {
+    options() {
+      return {
+        onEachFeature: this.onEachFeatureFunction,
+      };
+    },
+    styleFunction() {
+      const fillColor = this.fillColor; // important! need touch fillColor in computed for re-calculate when change fillColor
+      return () => {
+        return {
+          weight: 2,
+          color: "#ECEFF1",
+          opacity: 1,
+          fillColor: fillColor,
+          fillOpacity: 1,
+        };
+      };
+    },
+    onEachFeatureFunction() {
+      if (!this.enableTooltip) {
+        return () => {};
+      }
+      return (feature, layer) => {
+        layer.bindTooltip(
+          "<div>code:" +
+            feature.properties.code +
+            "</div><div>nom: " +
+            feature.properties.nom +
+            "</div>",
+          { permanent: false, sticky: true }
+        );
+      };
+    },
+  },
+  async created() {
+    this.loading = true;
+    const response = await fetch(
+      "https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/communes-pays-de-la-loire.geojson"
+    );
+    const data = await response.json();
+    this.geojson = data;
+    this.loading = false;
   },
 };
 </script>
