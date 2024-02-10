3a340277e80b946a32d1697bc20a5adb6704059c
kufre-abasi <kufreabasibassey3@gmail.com>
2024-02-10 15:26:30 +0100

    docs: modified

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,73 +1,75 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue'
-import DashboardLayout from '@/components/layout/dashboardLayout.vue'
-import LogoIcon from '@/components/icons/logoIcon.vue'
-import JobCard from '@/components/ui/Jobs/JobCard.vue'
-import BusinessJobCard from '@/components/ui/Jobs/Business/JobCard.vue'
-import MyApplicationCard from '@/components/ui/MyApplicationCard.vue'
-import CommunityCard from '@/components/ui/CommunityCard.vue'
-import CourseCard from '@/components/ui/CourseCard.vue'
-import ArticleCard from '@/components/ui/ArticleCard.vue'
-import { useStore } from '@/stores/user'
-import { useUserProfile } from '@/stores/profile'
+import { ref, onMounted, computed } from "vue";
+import DashboardLayout from "@/components/layout/dashboardLayout.vue";
+import LogoIcon from "@/components/icons/logoIcon.vue";
+import JobCard from "@/components/ui/Jobs/JobCard.vue";
+import BusinessJobCard from "@/components/ui/Jobs/Business/JobCard.vue";
+import MyApplicationCard from "@/components/ui/MyApplicationCard.vue";
+import CommunityCard from "@/components/ui/CommunityCard.vue";
+import CourseCard from "@/components/ui/CourseCard.vue";
+import ArticleCard from "@/components/ui/ArticleCard.vue";
+import { useStore } from "@/stores/user";
+import { useUserProfile } from "@/stores/profile";
 // import OnboardingRequest from "@/components/ui/Onboarding/OnboardingRequest.vue";
-import BusinessValuesCard from '@/components/ui/Cards/BusinessValuesCard.vue'
-import JobsStatistics from '@/components/ui/Jobs/Business/JobsStatistics.vue'
+import BusinessValuesCard from "@/components/ui/Cards/BusinessValuesCard.vue";
+import JobsStatistics from "@/components/ui/Jobs/Business/JobsStatistics.vue";
 // import PagePreLoader from "@/components/ui/Loader/PagePreLoader.vue";
-import { useTabStore } from '@/stores/tab'
-import { useRouter } from 'vue-router'
-import { storeToRefs } from 'pinia'
-import { useJobsStore } from '@/stores/jobs'
-const JobsStore = useJobsStore()
-const { myJobsApplications } = storeToRefs(JobsStore)
+import { useTabStore } from "@/stores/tab";
+import { useRouter } from "vue-router";
+import { storeToRefs } from "pinia";
+import { useJobsStore } from "@/stores/jobs";
+const JobsStore = useJobsStore();
+const { myJobsApplications } = storeToRefs(JobsStore);
 
-const router = useRouter()
+const router = useRouter();
 
-const tabStore = useTabStore()
-const { isLoading } = storeToRefs(tabStore)
+const tabStore = useTabStore();
+const { isLoading } = storeToRefs(tabStore);
 
-let store = useStore()
-let profile = useUserProfile()
+let store = useStore();
+let profile = useUserProfile();
 // const showPageLoader = ref(true);
 
-const isOnBoarded = computed(() => profile.user)
+const isOnBoarded = computed(() => profile.user);
 
 const userDetails = computed(() => {
-  return profile.user.data
-})
+  return profile.user.data;
+});
 const accountType = computed(() => {
-  return store.getUser.data.user.type
-})
+  return store.getUser.data.user.type;
+});
 onMounted(() => {
-  return profile.userProfile()
-})
+  return profile.userProfile();
+});
 onMounted(() => {
-  return accountType
-})
+  return accountType;
+});
 onMounted(async () => {
   try {
-    await profile.userProfile()
+    await profile.userProfile();
     if (
       isOnBoarded.value &&
       !isOnBoarded.value.business_details &&
       !isOnBoarded.value.work_details
     ) {
-      if (accountType.value === 'talent') {
-        router.push({ name: 'talent-onboarding' })
-      } else if (accountType.value === 'business') {
-        router.push({ name: 'business-onboarding' })
+      if (accountType.value === "talent") {
+        router.push({ name: "talent-onboarding" });
+      } else if (accountType.value === "business") {
+        router.push({ name: "business-onboarding" });
       }
     }
   } catch (error) {
     /* empty */
   } finally {
-    isLoading.value = !isLoading.value
+    isLoading.value = !isLoading.value;
   }
-})
-//!isOnBoarded.portofolio;
+});
+//!isOnBoarded.portofoolio;
 onMounted(async () => {
-  await JobsStore.handleMyJobApplications()
-})
+  if (accountType.value === "talent") {
+    await JobsStore.handleMyJobApplications();
+  }
+});
 </script>
 
 <template>
@@ -97,15 +99,15 @@ onMounted(async () => {
               v-if="accountType === 'talent'"
               class="text-[#011B1F] text-[12.299px] leading-[16.707px] font-Satoshi400"
             >
-              You now have full access to our network of job openings and thriving community. Are
-              you ready to take on new opportunities?
+              You now have full access to our network of job openings and thriving
+              community. Are you ready to take on new opportunities?
             </p>
             <p
               v-if="accountType !== 'talent'"
               class="text-[#011B1F] text-[12.299px] leading-[16.707px] font-Satoshi400"
             >
-              You now have full access to our network of talents and thriving community. Are you
-              ready to start hiring?
+              You now have full access to our network of talents and thriving community.
+              Are you ready to start hiring?
             </p>
           </div>
           <div class="flex gap-4 mt-6">
@@ -156,7 +158,9 @@ onMounted(async () => {
           class="p-4 px-6 flex flex-col gap-3 justify-between rounded-[4.533px] w-full bg-[#EDF0B8] border-[0.567px] border-[#254035AB]"
         >
           <div>
-            <p class="text-[#244034] lg:text-[26.2px] text-[19px] leading-[51.2px] font-Satoshi500">
+            <p
+              class="text-[#244034] lg:text-[26.2px] text-[19px] leading-[51.2px] font-Satoshi500"
+            >
               Verify your account details
             </p>
             <p class="text-[#011B1F] text-[12.299px] leading-[17.284px] font-Satoshi400">
@@ -208,18 +212,25 @@ onMounted(async () => {
       </div>
 
       <!-- Jobs Statistics -->
-      <div v-if="accountType !== 'talent'" class="flex lg:flex-row flex-col gap-[32.12px] my-8">
+      <div
+        v-if="accountType !== 'talent'"
+        class="flex lg:flex-row flex-col gap-[32.12px] my-8"
+      >
         <JobsStatistics class="min-w-[95%] lg:min-w-[65.9%]" />
         <div class="flex flex-col gap-4 w-full">
           <div
             class="p-[28.26px] px-[22.48px] rounded-[4.533px] w-full bg-[#FFF] flex flex-row justify-between h-full border-[0.567px] border-[#254035AB]"
           >
             <div class="flex flex-col gap-3 h-full w-full">
-              <h4 class="text-[#244034] leading-[17.646px] font-Satoshi700 text-[23.467px]">
+              <h4
+                class="text-[#244034] leading-[17.646px] font-Satoshi700 text-[23.467px]"
+              >
                 Jobs Open
               </h4>
               <div class="flex flex-row gap-2">
-                <h4 class="text-[#244034] font-Satoshi700 leading-none text-[60.722px]">12</h4>
+                <h4 class="text-[#244034] font-Satoshi700 leading-none text-[60.722px]">
+                  12
+                </h4>
                 <div class="flex flex-col h-full justify-between">
                   <div class="flex flex-row"></div>
                   <p
@@ -235,11 +246,15 @@ onMounted(async () => {
             class="p-[28.26px] px-[22.48px] rounded-[4.533px] w-full h-full bg-[#FFF] border-[0.567px] border-[#254035AB]"
           >
             <div class="flex flex-col gap-3 h-full w-full">
-              <h4 class="text-[#244034] leading-[17.646px] font-Satoshi700 text-[23.467px]">
+              <h4
+                class="text-[#244034] leading-[17.646px] font-Satoshi700 text-[23.467px]"
+              >
                 Applicants Summary
               </h4>
               <div class="flex flex-row gap-2">
-                <h4 class="text-[#244034] font-Satoshi700 leading-none text-[60.722px]">69</h4>
+                <h4 class="text-[#244034] font-Satoshi700 leading-none text-[60.722px]">
+                  69
+                </h4>
                 <div class="flex flex-col h-full justify-between">
                   <div class="flex flex-row"></div>
                   <p
@@ -260,13 +275,17 @@ onMounted(async () => {
 
           <router-link
             class="text-[#011B1F] border-b-[1px] flex items-center border-b-[#011B1F] font-Satoshi500 text-[12.299px]"
-            to="/jobs"
+            to="/job-lists"
             >View all jobs</router-link
           >
         </div>
 
         <div class="flex gap-3 overflow-x-auto hide-scrollbar my-8">
-          <BusinessJobCard class="min-w-[95%] lg:min-w-[40%]" v-for="item in 4" :key="item" />
+          <BusinessJobCard
+            class="min-w-[95%] lg:min-w-[40%]"
+            v-for="item in 4"
+            :key="item"
+          />
         </div>
       </div>
 
@@ -340,7 +359,9 @@ onMounted(async () => {
       </div>
       <!-- articles -->
       <div class="mt-10 overflow-x-auto">
-        <p class="text-[18px] font-Satoshi400 !mb-[60px] text-[#244034]">MySpurr Articles</p>
+        <p class="text-[18px] font-Satoshi400 !mb-[60px] text-[#244034]">
+          MySpurr Articles
+        </p>
         <div class="grid md:grid-cols-2 grid-cols-1 gap-3">
           <ArticleCard class="" v-for="item in 4" :key="item" />
         </div>
