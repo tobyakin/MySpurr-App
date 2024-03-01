d1d2b1d6064194c5d470a21b1c87cd4e455d9c5d
kufre-abasi <kufreabasibassey3@gmail.com>
2024-03-02 00:40:02 +0100

    docs: modified

--- src/components/ui/JobListing/JobsCard.vue
+++ src/components/ui/JobListing/JobsCard.vue
@@ -123,7 +123,7 @@ const deleteJob = async (id) => {
             </div>
           </div>
           <div class="text-right flex justify-end cursor-pointer relative pr-4">
-            <button @click="toggleDocument(job)">
+            <button class="px-4" @click="toggleDocument(job)">
               <svg
                 class="cursor-pointer flex lg:flex-row flex-col gap-4 justify-end"
                 xmlns="http://www.w3.org/2000/svg"
