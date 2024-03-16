2e80e50b1c2b2e6d68c9c3428c967c5014718d30
kufre-abasi <kufreabasibassey3@gmail.com>
2024-03-16 16:29:22 +0100

    feat: my schedule

--- src/components/SideBar/index.vue
+++ src/components/SideBar/index.vue
@@ -125,12 +125,14 @@
       </router-link>
       <router-link
         v-if="feature.includes('MY_SCHEDULE')"
-        to="/dashboard"
+        to="/my-schedule"
         class="p-[10px] flex justify-start hover:bg-brand/[0.1] rounded-[5px]"
-        :class="route.name === '' ? 'text-white bg-[#43D0DF]' : 'text-[#01272C] '"
+        :class="
+          route.name === 'my-schedule' ? 'text-white bg-[#43D0DF]' : 'text-[#01272C] '
+        "
       >
         <div
-          :class="route.name === '' ? 'text-white ' : 'text-[#297F88] '"
+          :class="route.name === 'my-schedule' ? 'text-white ' : 'text-[#297F88] '"
           class="mr-[20px] justify-center flex items-center rounded-[5px] h-[40px] w-[40px]"
         >
           <MySchedule />
