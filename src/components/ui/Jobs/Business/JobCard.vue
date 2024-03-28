a47f5a97ba8f49f9d0bd4ae0ed101c3f1f810c1a
kufre-abasi <kufreabasibassey3@gmail.com>
2024-03-28 17:29:34 +0100

    docs: image shape

--- src/components/ui/Jobs/Business/JobCard.vue
+++ src/components/ui/Jobs/Business/JobCard.vue
@@ -13,31 +13,45 @@ defineProps({
 });
 </script>
 <template>
-  <div class="border-[#254035AB] border-[0.735px] rounded-[7.347px] p-4">
+  <div
+    class="border-[#254035AB] border-[0.735px] relative rounded-[7.347px] h-[54vh] p-4"
+  >
     <div class="flex items-center gap-3 mb-5">
-      <img
-        class="h-[60px] w-[71px] rounded-full object-contain bg-brand"
-        :src="job?.company.company_logo ? job?.company.company_logo : defultLogo"
-        alt=""
-      />
+      <div class="w-[50px] h-[50px] bg-[#EAEAEA] rounded-full object-contain">
+        <img
+          class="h-[50px] w-[50px] rounded-full object-cover bg-brand"
+          :src="job?.company.company_logo ? job?.company.company_logo : defultLogo"
+          alt=""
+        />
+      </div>
       <div class="flex justify-between items-center w-full">
         <p class="text-[13.021px] font-Satoshi500 text-[#2F929C]">
           {{ job?.company.business_name }}
         </p>
         <div
-          class="bg-[#EDF0B8] font-Satoshi500 text-[9.708px] p-3 px-6 text-[#000000] rounded-full"
+          class="bg-[#EDF0B8] font-Satoshi500 text-[10.708px] capitalize p-2 px-4 text-[#000000] rounded-full"
         >
           {{ job?.job_type }}
         </div>
       </div>
     </div>
     <hr class="text-[#EAEAEA]" />
-    <div class="flex flex-col justify-between mt-5">
+    <div class="flex flex-col justify-normal lg:h-[35vh] overflow-hidden mt-5">
       <div class="flex justify-between">
-        <div class="flex flex-col gap-2">
-          <p class="!text-[20.419px] font-Satoshi500 text-[#000000]">
-            {{ job?.job_title }}
-          </p>
+        <div class="flex flex-col w-full gap-2">
+          <div class="flex flex-row justify-between gap-2">
+            <p class="!text-[20.419px] font-Satoshi500 text-[#000000]">
+              {{ job?.job_title }}
+            </p>
+            <p
+              class="text-[10.988px] capitalize font-Satoshi500 flex items-center text-[#244034B2]"
+            >
+              <span v-html="numAbbr.formatCurrency(job?.currency)"></span>
+
+              {{ store.abbr(job?.salary_min) }}- {{ store.abbr(job?.salary_max) }}/
+              {{ job?.salaray_type }}
+            </p>
+          </div>
           <div class="flex gap-2">
             <div
               class="flex gap-1 items-center text-[10.334px] text-[#DA5252] font-Satoshi500"
@@ -58,39 +72,30 @@ defineProps({
             </div>
           </div>
         </div>
-        <div>
-          <!-- <p class="text-[8.988px] font-Satoshi500 text-[#244034B2]">$30-$50/hour</p> -->
-          <p
-            class="text-[8.988px] capitalize font-Satoshi500 flex items-center text-[#244034B2]"
+      </div>
+      <div class="flex flex-col justify-between h-full">
+        <div class="flex gap-4 flex-wrap my-[17.512px]">
+          <div
+            v-for="i in job?.skills"
+            :key="i"
+            class="bg-[#FBEEEE] font-Satoshi500 text-[9.708px] p-2 px-4 text-[#000000] rounded-full"
           >
-            <span v-html="numAbbr.formatCurrency(job?.currency)"></span>
-
-            {{ store.abbr(job?.salary_min) }}- {{ store.abbr(job?.salary_max) }}/
-            {{ job?.salaray_type }}
-          </p>
+            {{ i.name }}
+          </div>
         </div>
-      </div>
-      <div class="flex gap-4 flex-wrap my-[17.512px]">
-        <div
-          v-for="i in job?.skills"
-          :key="i"
-          class="bg-[#FBEEEE] font-Satoshi500 text-[9.708px] p-2 px-6 text-[#000000] rounded-full"
-        >
-          {{ i.name }}
-        </div>
-      </div>
-      <div class="flex flex-col gap-2 items- text-left mt-[2px]">
-        <!-- <div class="bg-[#E6F1F3] rounded-lg w-full">
+        <div class="flex flex-col gap-2 text-left mt-[2px]">
+          <!-- <div class="bg-[#E6F1F3] rounded-lg w-full">
           <div
             :style="`width: ${parseInt((50 / 100) * 100)}%`"
             class="h-[7px] rounded-lg bg-[#2B8C97]"
           ></div>
         </div> -->
-        <p class="text-[10.252px] font-Satoshi500 leading-[15.6px] text-[#000000]">
-          {{ job?.applicants }} Applied
+          <p class="text-[10.252px] font-Satoshi500 leading-[15.6px] text-[#000000]">
+            {{ job?.applicants }} Applied
 
-          <!-- <span class="text-[#6C8285]">of 10 Capacity</span> -->
-        </p>
+            <!-- <span class="text-[#6C8285]">of 10 Capacity</span> -->
+          </p>
+        </div>
       </div>
     </div>
   </div>
