917cb77cb3c79b80034fa10e08f0baf83f419a31
kufre-abasi <kufreabasibassey3@gmail.com>
2024-04-01 16:35:34 +0100

    feat: loader

--- src/views/Profile/PublicProfile.vue
+++ src/views/Profile/PublicProfile.vue
@@ -1,38 +1,41 @@
 <script setup>
-import { onUnmounted } from "vue";
-import DashboardLayout from "@/components/layout/dashboardLayout.vue";
-import TalentPublicProfile from "@/components/ui/FindTalents/TalentPublicProfile.vue";
-import { useTalentsStore } from "@/stores/talents";
-import { storeToRefs } from "pinia";
-import { useQuery } from "vue-query";
-import { useRoute } from "vue-router";
-const talentsStore = useTalentsStore();
-const { singleTalent } = storeToRefs(talentsStore);
-const route = useRoute();
+import { onUnmounted } from 'vue'
+import DashboardLayout from '@/components/layout/dashboardLayout.vue'
+import TalentPublicProfile from '@/components/ui/FindTalents/TalentPublicProfile.vue'
+import { useTalentsStore } from '@/stores/talents'
+import { storeToRefs } from 'pinia'
+import { useQuery } from 'vue-query'
+import { useRoute } from 'vue-router'
+const talentsStore = useTalentsStore()
+const { singleTalent } = storeToRefs(talentsStore)
+const route = useRoute()
 const getTallentsDetails = async () => {
-  let response = await talentsStore.handleGetSingleTalent(route.params.uuid);
-  return response;
-};
+  let response = await talentsStore.handleGetSingleTalent(route.params.uuid)
+  return response
+}
 const fetchData = async () => {
-  await Promise.all([getTallentsDetails()]);
-};
+  await Promise.all([getTallentsDetails()])
+}
 
-fetchData();
+fetchData()
 
-useQuery(["myJobs"], getTallentsDetails, {
+const { isLoading } = useQuery(['myJobs'], getTallentsDetails, {
   retry: 10,
   staleTime: 10000,
   onSuccess: (data) => {
-    singleTalent.value = data;
-  },
-});
+    singleTalent.value = data
+  }
+})
 onUnmounted(() => {
-  singleTalent.value = null;
-});
+  singleTalent.value = null
+})
 // onMounted(async () => {
 //   await talentsStore.handleGetSingleTalent(route.params.uuid);
 // });
 </script>
 <template>
-  <DashboardLayout><TalentPublicProfile :talent="singleTalent?.data" /></DashboardLayout>
+  <DashboardLayout>
+    <ShortLoader v-if="isLoading" />
+    <TalentPublicProfile v-else :talent="singleTalent?.data"
+  /></DashboardLayout>
 </template>
