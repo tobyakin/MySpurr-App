4d7caf236063893d42b1bc191374d7d69581fc49
kufre-abasi <kufreabasibassey3@gmail.com>
2024-04-11 22:43:53 +0100

    docs: add portfolio modifed

--- src/views/Portfolio/AddPortfolio.vue
+++ src/views/Portfolio/AddPortfolio.vue
@@ -69,7 +69,7 @@ const selectOption = (option) => {
     search.value = "";
     showDropdown.value = false;
     highlightedIndex.value = -1;
-    portfolio.value.tags.push(option);
+    portfolio.value.tags.push(option.name);
   }
 };
 
@@ -108,29 +108,29 @@ const selectHighlightedOption = () => {
 const isFormValid = computed(() => {
   return (
     portfolio.value.title !== "" &&
-    portfolio.value.client_name !== "" &&
-    portfolio.value.job_type !== "" &&
-    portfolio.value.location !== "" &&
+    portfolio.value.category_id !== "" &&
+    portfolio.value.description !== "" &&
     portfolio.value.tags.length > 0 &&
-    portfolio.value.cover_image !== null &&
-    portfolio.value.body.trim() !== "" &&
-    portfolio.value.min_rate.trim() !== "" &&
-    portfolio.value.max_rate.trim() !== ""
+    portfolio.value.project_image.length > 0 &&
+    portfolio.value.link !== "" &&
+    portfolio.value.featured_image !== null
   );
 });
 
 // upload image
 const uploadedImageName = ref("");
+const uploadedFeatureImageName = ref("");
 const restForm = () => {
   (portfolio.value.title = ""),
-    (portfolio.value.client_name = ""),
-    (portfolio.value.job_type = ""),
-    (portfolio.value.location = ""),
+    (portfolio.value.category_id = ""),
+    (portfolio.value.description = ""),
     (portfolio.value.tags = []),
-    (portfolio.value.cover_image = null),
-    (portfolio.value.body = ""),
-    (portfolio.value.min_rate = ""),
-    (portfolio.value.max_rate = "");
+    (portfolio.value.project_image = Array.from({ length: 4 }, () => ({
+      name: null,
+      image: null,
+    }))),
+    (portfolio.value.link = ""),
+    (portfolio.value.featured_image = "");
 };
 
 // const uploadImage = (event) => {
@@ -152,7 +152,7 @@ const restForm = () => {
 //     portfolio.value.cover_image = "";
 //   }
 // };
-const uploadImage = (event) => {
+const uploadImage = (event, index) => {
   const file = event.target.files[0];
 
   if (file) {
@@ -191,19 +191,73 @@ const uploadImage = (event) => {
         const reducedQualityDataURL = canvas.toDataURL("image/jpeg", 0.4); // Adjust quality as needed
 
         // Assign reduced quality image to portfolio.cover_image
-        portfolio.value.cover_image = reducedQualityDataURL;
+        portfolio.value.project_image[index].image = reducedQualityDataURL;
+        portfolio.value.project_image[index].name = file.name;
       };
     };
     reader.readAsDataURL(file);
   } else {
-    portfolio.value.cover_image = "";
+    portfolio.value.project_image[index].image = null;
+    portfolio.value.project_image[index].name = null;
   }
 };
+const uploadFeatureImage = (event) => {
+  const file = event.target.files[0];
 
-const removeImage = () => {
-  portfolio.value.cover_image = null;
-  uploadedImageName.value = "";
+  if (file) {
+    const reader = new FileReader();
+    uploadedFeatureImageName.value = file.name;
+
+    reader.onload = () => {
+      const img = new Image();
+      img.src = reader.result;
+
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        const maxSizeMB = 10; // Maximum allowed file size in MB
+        const maxDimension = 1200; // Maximum width or height of the image
+
+        let width = img.width;
+        let height = img.height;
+
+        // Check if image size exceeds the maximum allowed size
+        let scaleFactor = 1;
+        if (file.size / (1024 * 1024) > maxSizeMB) {
+          scaleFactor = Math.min(maxDimension / width, maxDimension / height);
+          width *= scaleFactor;
+          height *= scaleFactor;
+        }
+
+        // Set canvas dimensions
+        canvas.width = width;
+        canvas.height = height;
+
+        // Draw image on canvas
+        ctx.drawImage(img, 0, 0, width, height);
+
+        // Convert canvas to base64 data URL with reduced quality
+        const reducedQualityDataURL = canvas.toDataURL("image/jpeg", 0.4); // Adjust quality as needed
+
+        // Assign reduced quality image to portfolio.cover_image
+        portfolio.value.featured_image = reducedQualityDataURL;
+      };
+    };
+    reader.readAsDataURL(file);
+  } else {
+    portfolio.value.featured_image = "";
+  }
 };
+const removeImage = (index) => {
+  portfolio.value.project_image[index].image = null;
+  portfolio.value.project_image[index].name = null;
+  // uploadedImageName.value = "";
+};
+const removeFeatureImage = () => {
+  portfolio.value.featured_image = null;
+  uploadedFeatureImageName.value = "";
+};
+
 const onFinish = async () => {
   loading.value = true;
   try {
@@ -245,17 +299,53 @@ const saveAsDraft = async () => {
         Project details
       </h4>
       <div class="mt-8 flex flex-col gap-[49px]">
-        <FormGroup
-          v-model="portfolio.title"
-          labelClasses="font-Satoshi500 !text-[17.792px]"
-          label="Title"
-          name="Name"
-          placeholder="Vino brand identity"
-          type="text"
-          inputClasses="w-full mt-2 font-light font-Satoshi400 !p-3 border-[#EDEDED] border-[0.509px] opacity-[0.8029] rounded-[5.897px] text-[12.68px]"
-        ></FormGroup>
         <div class="flex lg:flex-row flex-col w-full gap-8">
-          <SelectGroup
+          <FormGroup
+            v-model="portfolio.title"
+            labelClasses="font-Satoshi500 !text-[17.792px]"
+            label="Title"
+            name="Name"
+            placeholder="Vino brand identity"
+            type="text"
+            inputClasses="w-full mt-2 font-light font-Satoshi400 !p-3 !py-2.5 !border-[#254035AB] border-[0.509px] opacity-[0.8029] rounded-[5.897px] text-[12.68px]"
+          ></FormGroup>
+          <div class="flex flex-col w-full text-left">
+            <Label class="font-Satoshi500 !text-[17.792px] mb-2.5">Category</Label>
+            <a-select
+              placeholder=""
+              :show-arrow="true"
+              :bordered="true"
+              class="w-full !outline-none !px-1"
+              show-search
+              v-model:value="portfolio.category_id"
+            >
+              <a-select-option
+                v-for="item in [
+                  { id: 0, name: 'Brand Identity Design ' },
+                  { id: 1, name: 'Logo Design' },
+                  { id: 2, name: 'Graphic Design' },
+                ]"
+                :key="item.id"
+                :value="item.id"
+              >
+                {{ item.name }}
+              </a-select-option>
+            </a-select>
+          </div>
+
+          <!-- <SelectGroup
+            v-model="portfolio.category_id"
+            labelClasses="font-Satoshi500 !text-[17.792px]"
+            label="Category"
+            name="Name"
+            :items="['Brand Identity Design ', 'Logo Design', 'Graphic Design']"
+            placeholder="Category"
+            type="text"
+            inputClasses="w-full mt-2 font-light bg-white font-Satoshi400 !p-2 !border-[#254035AB] border-[0.509px] opacity-[0.8029] rounded-[5.897px] text-[12.68px]"
+          ></SelectGroup> -->
+        </div>
+        <!-- <div class="flex lg:flex-row flex-col w-full gap-8"> -->
+        <!-- <SelectGroup
             v-model="portfolio.client_name"
             labelClasses="font-Satoshi500 text-[15.606px]"
             label="Creative work"
@@ -264,8 +354,8 @@ const saveAsDraft = async () => {
             placeholder="Creative work"
             type="text"
             inputClasses="w-full mt-2 font-light bg-white font-Satoshi400 !p-2 border-[#EDEDED] border-[0.509px] opacity-[0.8029] rounded-[5.897px] text-[12.68px]"
-          ></SelectGroup>
-          <SelectGroup
+          ></SelectGroup> -->
+        <!-- <SelectGroup
             v-model="portfolio.job_type"
             labelClasses="font-Satoshi500 text-[15.606px]"
             label="Employment type"
@@ -274,9 +364,9 @@ const saveAsDraft = async () => {
             placeholder="Employment type"
             type="text"
             inputClasses="w-full mt-2 font-light bg-white font-Satoshi400 !p-2 border-[#EDEDED] border-[0.509px] opacity-[0.8029] rounded-[5.897px] text-[12.68px]"
-          ></SelectGroup>
-        </div>
-        <div class="flex lg:flex-row flex-col w-full gap-8">
+          ></SelectGroup> -->
+        <!-- </div> -->
+        <!-- <div class="flex lg:flex-row flex-col w-full gap-8">
           <div class="lg:w-[50%]">
             <FormGroup
               v-model="portfolio.location"
@@ -307,21 +397,109 @@ const saveAsDraft = async () => {
               inputClasses="w-full mt-2 font-light font-Satoshi400 !p-[10px] border-[#EDEDED] border-[0.509px] opacity-[0.8029] rounded-[5.897px] text-[12.68px]"
             ></FormGroup>
           </div>
-        </div>
+        </div> -->
       </div>
-      <h4 class="text-[#2B7551] font-Satoshi500 text-[33.212px] mt-[64.05px]">
-        Start building your project
+      <h4 class="text-[#2B7551] font-Satoshi500 text-[33.212px] mt-[44.05px]">
+        Describe your project (300 words)
       </h4>
       <div class="mt-8 flex flex-col h-[58vh]">
         <QuillEditor
-          v-model:content="portfolio.body"
+          v-model:content="portfolio.description"
           class=""
           theme="snow"
-          toolbar="full"
           placeholder="Write about the job in details..."
           contentType="html"
         />
       </div>
+      <h4 class="text-[#2B7551] font-Satoshi500 text-[28.468px] mt-[44.05px]">
+        Upload up to 4 project images
+      </h4>
+      <div class="mt-8 flex flex-col gap-8">
+        <Label class="font-Satoshi500 text-[15.606px]">File Attachment*</Label>
+        <div
+          v-for="(image, index) in portfolio.project_image"
+          :key="index"
+          class="flex flex-col gap-4"
+        >
+          <div
+            v-if="image.image"
+            class="w-full bg-[#EDF2F7] flex flex-row items-center justify-between rounded-[5.897px] py-[20px] px-[28px] text-[#000000] text-[16.606px] font-Satoshi400"
+          >
+            <p>{{ image.name }}</p>
+            <button
+              @click="removeImage(index)"
+              class="origin-center rotate-45 text-[28.468px] font-Satoshi400 text-[#3F634D]"
+            >
+              +
+            </button>
+          </div>
+          <img
+            v-if="image.image"
+            :src="image.image"
+            alt="Uploaded Image"
+            class="rounded-[5.897px] object-cover h-[300px] w-full"
+          />
+
+          <div v-if="!image.image" class="flex lg:flex-row flex-col gap-2 items-center">
+            <input
+              :id="'cover_image_' + index"
+              hidden
+              type="file"
+              @change="uploadImage($event, index)"
+            />
+            <label
+              :for="'cover_image_' + index"
+              class="bg-[#3F634D33] px-8 p-3 cursor-pointer rounded-[8.303px] text-[17.792px] text-[#3F634D] font-Satoshi500"
+              ><span> + Upload File</span></label
+            >
+            <p class="text-[#00000080] font-Satoshi400 text-[16.606px]">
+              Upload file .jpeg, .png, .svg
+            </p>
+          </div>
+        </div>
+      </div>
+      <h4 class="text-[#2B7551] font-Satoshi500 text-[28.468px] mt-[64.05px]">
+        Upload featured image
+      </h4>
+      <div class="mt-8 flex flex-col gap-8">
+        <Label class="font-Satoshi500 text-[15.606px]">File Attachment*</Label>
+        <div class="flex flex-col gap-4" f>
+          <div
+            v-if="portfolio.featured_image"
+            class="w-full bg-[#EDF2F7] flex flex-row items-center justify-between rounded-[5.897px] py-[20px] px-[28px] text-[#000000] text-[16.606px] font-Satoshi400"
+          >
+            <p>{{ uploadedFeatureImageName }}</p>
+            <button
+              @click="removeFeatureImage"
+              class="origin-center rotate-45 text-[28.468px] font-Satoshi400 text-[#3F634D]"
+            >
+              +
+            </button>
+          </div>
+          <img
+            v-if="portfolio.featured_image"
+            :src="portfolio?.featured_image"
+            alt="Uploaded Image"
+            class="rounded-[5.897px] object-cover h-[300px] w-full"
+          />
+
+          <div
+            v-if="!portfolio.featured_image"
+            class="flex lg:flex-row flex-col gap-2 items-center"
+          >
+            <input id="featured_image" hidden type="file" @change="uploadFeatureImage" />
+            <label
+              for="featured_image"
+              class="bg-[#3F634D33] px-8 p-3 cursor-pointer rounded-[8.303px] text-[17.792px] text-[#3F634D] font-Satoshi500"
+              ><span> + Upload File</span></label
+            >
+            <p class="text-[#00000080] font-Satoshi400 text-[16.606px]">
+              Upload file .jpeg, .png, .svg
+            </p>
+          </div>
+        </div>
+      </div>
+
       <h4 class="text-[#2B7551] font-Satoshi500 text-[33.212px] mt-[64.05px]">Tags</h4>
       <div class="mt-8 flex flex-col gap-8">
         <div class="border-[0.737px] border-[#254035AB] rounded-[5.897px] p-4 py-1.5">
@@ -332,10 +510,10 @@ const saveAsDraft = async () => {
             <div class="selected-items p-2 gap-2">
               <div
                 v-for="(selectedItem, index) in portfolio.tags"
-                :key="selectedItem.id"
+                :key="selectedItem"
                 class="selected-item bg-[#31795A1A] text-sm font-Satoshi400 gap-2 px-4 p-[5px] text-[#0000008A] !rounded-full"
               >
-                {{ selectedItem.name }}
+                {{ selectedItem }}
                 <span
                   @click="removeSelectedItem(index)"
                   class="remove-btn text-black hover:text-red-500"
@@ -375,44 +553,21 @@ const saveAsDraft = async () => {
           </div>
         </div>
       </div>
-      <h4 class="text-[#2B7551] font-Satoshi500 text-[28.468px] mt-[64.05px]">
-        Featured image
+      <h4 class="text-[#2B7551] font-Satoshi500 text-[33.212px] mt-[64.05px]">
+        Share a Link to full project
       </h4>
-      <div class="mt-8 flex flex-col gap-8">
-        <Label class="font-Satoshi500 text-[15.606px]">File Attachment*</Label>
-        <div class="flex flex-col gap-4" f>
-          <div
-            v-if="portfolio.cover_image"
-            class="w-full bg-[#EDF2F7] flex flex-row items-center justify-between rounded-[5.897px] py-[20px] px-[28px] text-[#000000] text-[16.606px] font-Satoshi400"
-          >
-            <p>{{ uploadedImageName }}</p>
-            <button
-              @click="removeImage"
-              class="origin-center rotate-45 text-[28.468px] font-Satoshi400 text-[#3F634D]"
-            >
-              +
-            </button>
-          </div>
-          <img
-            v-if="portfolio.cover_image"
-            :src="portfolio?.cover_image"
-            alt="Uploaded Image"
-            class="rounded-[5.897px]"
-          />
 
-          <div class="flex lg:flex-row flex-col gap-2 items-center">
-            <input id="cover_image" hidden type="file" @change="uploadImage" />
-            <label
-              for="cover_image"
-              class="bg-[#3F634D33] px-8 p-3 cursor-pointer rounded-[8.303px] text-[17.792px] text-[#3F634D] font-Satoshi500"
-              ><span>Upload File</span></label
-            >
-            <p class="text-[#00000080] font-Satoshi400 text-[16.606px]">
-              Upload file .jpeg, .png, .svg
-            </p>
-          </div>
-        </div>
+      <div class="w-full mt-8 flex flex-col gap-8">
+        <FormGroup
+          v-model="portfolio.link"
+          labelClasses="font-Satoshi500 text-[15.606px]"
+          label=""
+          placeholder=""
+          type="text"
+          inputClasses="w-full mt-2 font-light font-Satoshi400 !p-3 !border-[#254035AB] !border-[0.909px] opacity-[0.8029] rounded-[5.897px] text-[12.68px]"
+        ></FormGroup>
       </div>
+
       <div class="flex gap-4 justify-start mt-12">
         <button
           @click="saveAsDraft"
