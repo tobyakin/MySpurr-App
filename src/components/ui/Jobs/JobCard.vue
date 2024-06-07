91767cac9ecbc9f141d489f6b4e0b10a660acf2a
kufre-abasi <kufreabasibassey3@gmail.com>
2024-06-07 21:53:53 +0100

    docs: logo

--- src/components/ui/Jobs/JobCard.vue
+++ src/components/ui/Jobs/JobCard.vue
@@ -48,7 +48,7 @@ const copyUrl = () => {
 <template>
   <div class="border-[#254035AB] bg-white border-[0.735px] rounded-[7.347px] p-4">
     <div class="flex gap-3 mb-5">
-      <img :src="props?.job?.company?.logo" alt="" />
+      <img src="@/assets/image/jobIcon.svg.logo" alt="" />
       <div>
         <p class="text-[13.021px] font-Satoshi500 text-[#2F929C]">
           {{ props?.job?.company?.business_name }}
