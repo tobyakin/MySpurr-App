13c00858186fca9d52aad89350ac89fc6a3f08ff
Salawudeen Abdultawab <abdultawabsalawudeen@gmail.com>
2024-07-08 12:38:31 +0100

    Event UI: Added meta tags for social sharing

--- src/components/ui/SocialShare.vue
+++ src/components/ui/SocialShare.vue
@@ -21,14 +21,30 @@
    import LinkedinIcon from "@/components/icons/eventLinkedinIcon.vue"
    import twitterIcon from "@/components/icons/eventTwitterIcon.vue";
    import { ref } from "vue";
+   import { useHead } from '@vueuse/head';
 
     const title = ref('MySpurr');
     const description = ref('Career Coaching for Creatives: Your Path to Professional Excellence');
     const url = ref('https://www.myspurr.net/career-development/career-coaching-for-creatives-your-path-to-professional-excellence');
-    // const imageUrl = ref('https://ik.imagekit.io/ldtt3hq8g2/blog/1720275662__9fCtRfyN.jpg');
+    const imageUrl = ref('https://ik.imagekit.io/ldtt3hq8g2/blog/1720275662__9fCtRfyN.jpg');
     
     const encodedUrl = encodeURIComponent(url.value);
     const encodedMessage = encodeURIComponent(`${title.value} - ${description.value}`);
+
+    useHead({
+      title: title.value,
+      meta: [
+        { name: 'description', content: description.value },
+        { property: 'og:title', content: title.value },
+        { property: 'og:description', content: description.value },
+        { property: 'og:image', content: imageUrl.value },
+        { property: 'og:url', content: url.value },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: title.value },
+        { name: 'twitter:description', content: description.value },
+        { name: 'twitter:image', content: imageUrl.value }
+      ]
+    });
   </script>
   
   <style scoped>
