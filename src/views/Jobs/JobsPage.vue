5eafdd5f975eb117bd653f9d95840b2d856c948e
Salawudeen Abdultawab <abdultawabsalawudeen@gmail.com>
2024-11-26 19:16:30 +0100

    Jobs Page Filter

--- src/views/Jobs/JobsPage.vue
+++ src/views/Jobs/JobsPage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { watchEffect, ref, computed, onMounted, reactive, watch } from "vue";
+import { watchEffect, ref, computed, onMounted, reactive, watch, onUnmounted } from "vue";
 import { storeToRefs } from "pinia";
 import "vue-slider-component/theme/antd.css";
 import DashboardLayout from "@/components/layout/dashboardLayout.vue";
@@ -11,13 +11,21 @@ import FormGroup from "@/components/ui/Form/Input/FormGroup.vue";
 import Label from "@/components/ui/Form/Input/Label.vue";
 import { useRouter } from "vue-router";
 import ShortLoader from "@/components/ui/Loader/ShortLoader.vue";
+import { useSkillsStore } from "@/stores/skills";
+import { useStore } from "@/stores/user";
+import { useUserProfile } from "@/stores/profile";
+import filterBtnIcon from "@/components/icons/filterBtnIcon.vue"
+
 const loading = ref(false);
 const jobsStore = useJobsStore();
 const { Job } = storeToRefs(jobsStore);
-import { useStore } from "@/stores/user";
-import { useUserProfile } from "@/stores/profile";
-
 const router = useRouter();
+const showMobFilter = ref(false)
+const skillsStore = useSkillsStore();
+const { states, contriesCode, skills } = storeToRefs(skillsStore);
+const country = ref('Nigeria')
+
+
 const Experience = [
   { name: "Beginner ", year: "(1-2 yrs)" },
   { name: "Intermediate ", year: "(3-5 yrs)" },
@@ -53,9 +61,6 @@ let store = useStore();
 const accountType = computed(() => {
   return store.getUser.data.user.type;
 });
-onMounted(() => {
-  return accountType;
-});
 
 const profileStore = useUserProfile();
 
@@ -64,9 +69,6 @@ const { isLoading } = storeToRefs(tabStore);
 
 const isOnBoarded = computed(() => profileStore.user);
 
-onMounted(() => {
-  return accountType;
-});
 onMounted(async () => {
   try {
     await profileStore.userProfile();
@@ -131,7 +133,7 @@ const displayedPageNumbers = computed(() => {
 });
 
 const filteredJobs = computed(() => {
-  let filtered = Job?.value?.data; // Create a shallow copy of the jobs array
+  let filtered = Job.value?.data; // Create a shallow copy of the jobs array
 
   // Filtering based on the search criteria
   if (sortInput.name) {
@@ -139,45 +141,82 @@ const filteredJobs = computed(() => {
       item.job_title.toLowerCase().includes(sortInput.name.toLowerCase())
     );
   }
-  if (sortInput.jobType) {
-    filtered = filtered?.filter((item) =>
-      item.job_type.toLowerCase().includes(sortInput.jobType.toLowerCase())
-    );
+  if (sortInput.jobType && typeof sortInput.jobType === 'string' && sortInput.jobType !== "Job Type") {
+    filtered = filtered?.filter((item) => {
+      const itemJobType = item.job_type.toLowerCase().trim();
+      const inputJobType = sortInput.jobType.toLowerCase().trim();
+      
+      if(itemJobType.includes(inputJobType)) {
+        return item;
+      }
+    });
   }
 
-  if (sortInput.Location) {
-    filtered = filtered?.filter((item) =>
-      item.state.toLowerCase().includes(sortInput.Location.toLowerCase())
-    );
+  if (sortInput.Location && sortInput.Location !== "Select State") {
+    filtered = filtered?.filter((item) => {
+      const itemLocation = item.state.toLowerCase().trim();
+      const inputLocation = sortInput.Location.toLowerCase().trim();
+      
+      if(itemLocation.includes(inputLocation)) {
+        return item;
+      }
+    });
   }
 
-  if (sortInput.experienceLevel) {
-    filtered = filtered.filter((item) =>
-      item.experience.toLowerCase().includes(sortInput.experienceLevel.toLowerCase())
-    );
+  if (sortInput.experienceLevel && typeof sortInput.experienceLevel === 'string' && sortInput.experienceLevel !== "Experience") {
+    filtered = filtered?.filter((item) => {
+      const itemExperience = item.experience.toLowerCase().trim();
+      const inputExperience = sortInput.experienceLevel.toLowerCase().trim();
+      
+      if(itemExperience.includes(inputExperience)) {
+        return item;
+      }
+    });
   }
 
-  if (sortInput.Category) {
+  if (sortInput.Category && sortInput.Category !== "Search Skill Categories") {
     filtered = filtered.filter((item) =>
       item.skills.some((skill) =>
         skill.name.toLowerCase().includes(sortInput.Category.toLowerCase())
       )
     );
   }
+  if (sortInput.currency) {
+    filtered = filtered.filter((item) =>
+      item.currency.toLowerCase().includes(sortInput.currency.toLowerCase())
+    );
+  }
 
   // Filtering by Rate within the specified range
   if (rateMin.value || rateMax.value) {
     filtered = filtered.filter((item) => {
-      const rate = parseFloat(item.rate);
-      const min = rateMin.value ? parseFloat(rateMin.value) : Number.MIN_SAFE_INTEGER;
-      const max = rateMax.value ? parseFloat(rateMax.value) : Number.MAX_SAFE_INTEGER;
+      const salaryMin = parseFloat(item.salary_min);
+      const salaryMax = parseFloat(item.salary_max);
+      const min =
+        rateMin.value !== null ? parseFloat(rateMin.value) : Number.MIN_SAFE_INTEGER;
+      const max =
+        rateMax.value !== null ? parseFloat(rateMax.value) : Number.MAX_SAFE_INTEGER;
 
-      return rate >= min && rate <= max;
+      return salaryMax >= min && salaryMin <= max;
     });
   }
-
   return filtered;
 });
+
+const handleScreenResize = (event) => {
+  if (event.matches) {
+    sortInput.experienceLevel = "Experience";
+    sortInput.jobType = "Job Type";
+    sortInput.Location = "Select State"
+    sortInput.Category = "Search Skill Categories"
+  } else {
+    sortInput.experienceLevel = "";
+    sortInput.jobType = "";
+    sortInput.Category = "";
+    sortInput.Location = "";
+  }
+};
+
 const resetFilters = () => {
   sortInput.name = "";
   sortInput.jobType = "";
@@ -191,6 +230,33 @@ const resetFilters = () => {
 watch(currentPage, async (newPage) => {
   await jobsStore.allJobs(newPage)
 });
+
+
+const showFilter = ()=>{
+  showMobFilter.value = !showMobFilter.value
+
+  if (showMobFilter.value) {
+    sortInput.experienceLevel = "Experience";
+    sortInput.jobType = "Job Type";
+  } else {
+    sortInput.experienceLevel = "";
+    sortInput.candidateType = "";
+  }
+}
+
+const toggleFilter = ()=>{
+  showMobFilter.value = false
+}
+
+const isFilter = computed(()=>{
+  return sortInput.name.length > 0 || sortInput.Location !== "Select State" || sortInput.experienceLevel !== "Experience" || sortInput.jobType !== "Job Type" || sortInput.Category !== "Search Skill Categories"
+})
+
+const getCountryCode = async ()=>{
+  let payload = country.value
+  await skillsStore.handleGetStates(payload)
+}
+
 onMounted(async () => {
   loading.value = true;
   try {
@@ -209,6 +275,24 @@ watchEffect(() => {
     sortInput.name = searchQuery;
   }
 });
+
+onMounted(async () => {
+  const mediaQuery = window.matchMedia('(max-width: 1024px)');
+  mediaQuery.addEventListener('change', handleScreenResize);
+
+  handleScreenResize(mediaQuery);
+
+  onUnmounted(() => {
+    mediaQuery.removeEventListener('change', handleScreenResize);
+  });
+
+  let payload = "NG"
+  await skillsStore.handleGetStates(payload);
+  await skillsStore.getCountriesCode()
+  await skillsStore.getskills();
+  return accountType;
+});
+
 </script>
 
 <template>
@@ -218,7 +302,7 @@ watchEffect(() => {
         <template #tab1> All jobs </template>
         <template #view1>
           <div
-            class="bg-[#F3F5CD] rounded-[14.957px] flex flex-col gap-8 p-8 lg:px-12 border-[0.491px] border-[#97A6A8]"
+            class="bg-[#F3F5CD] rounded-[14.957px] flex flex-col gap-8 p-8 lg:px-12 border-[0.491px] border-[#97A6A8] eventBreak:hidden"
           >
             <div class="flex lg:flex-row flex-col gap-8">
               <FormGroup
@@ -392,6 +476,14 @@ watchEffect(() => {
               </button>
             </div>
           </div>
+          <div>
+            <button 
+            @click="showFilter"
+            class="items-center gap-[0.71rem] bg-[#31795A] text-[#fff] px-8 py-4 rounded-[2.4375rem] w-[9rem] btn-hover-2 hidden eventBreak:flex mob:py-4 mb-4">
+              <filterBtnIcon />
+              <span class="text-[1.13638rem] mob:text-[1rem] font-Satoshi500 leading-[ 2.55681rem]">Filter</span>
+            </button>
+          </div>
           <!-- <div class="mt-14 flex flex-col gap-8">
             <JobRowCard
               class="min-w-[95%] lg:min-w-[45%]"
@@ -401,7 +493,7 @@ watchEffect(() => {
             />
           </div> -->
           <ShortLoader v-if="loading" />
-          <div v-else class="mt-14 flex flex-col gap-8">
+          <div v-else class="mt-14 flex flex-col gap-8 eventBreak:mt-0">
             <JobRowCard
               class="min-w-[95%] lg:min-w-[45%]"
               v-for="item in filteredJobs"
@@ -456,6 +548,134 @@ watchEffect(() => {
               <Arrow />
             </button>
           </div> -->
+          <div class="mobile_filter fixed bottom-0 left-0 w-full overflow-y-auto hidden eventBreak:block transitionItem"
+          :class="showMobFilter? 'h-[70vh]': 'h-0'"
+          >
+            <section class="pb-[5.83rem] pt-[2rem] px-[1.94rem] bg-[#E9FAFB] rounded-t-[1.42044rem] msgTab:px-[1rem]">
+              <div class="mb-[3rem] flex justify-end">
+                <button class="border-gray-300 border p-[0.5rem] font-Satoshi700 cursor-pointer bg-[#fff] hover:scale-105 transitionItem px-4" @click="toggleFilter">X</button>
+              </div>
+              <div class="w-[80%] mx-auto flex flex-col gap-[2.1rem] msgTab:w-[90%]">
+                  <FormGroup
+                      v-model="sortInput.name"
+                      labelClasses="font-Satoshi500 text-[1.52rem]"
+                      label=""
+                      name="Name"
+                      placeholder="Search by keywords"
+                      type="text"
+                      inputClasses="w-full mt-2 font-light font-Satoshi400 !p-4 border-[#EDEDED] border-[0.509px] opacity-[0.8029] rounded-t-[6.828px] text-[0.88rem]"
+                      >
+                  </FormGroup>
+                  <div>
+                    <label for="location" class="font-Satoshi500 !text-[1rem]">Skills Categories</label>
+                    <div
+                      class="bg-[#fff] w-full mt-[0.5rem] font-light font-Satoshi400 !p-4 border-gray-300 border-[0.509px] opacity-[0.8029] rounded-[6.828px] text-[0.88rem]"
+                    >
+                      <select
+                        v-model="sortInput.Category"
+                        :bordered="false"
+                        :show-arrow="false"
+                        class="w-full !outline-none !px-0 cursor-pointer text-[#000000] font-Satoshi500 leading-[1.75rem]"
+                        show-search
+                      >
+      
+                        <option disabled value="Search Skill Categories" class="text-[1rem] font-Satoshi500">Search Skill Categories</option>
+                        <option v-for="skill in skills?.data" :key="skill.id" :value="skill.name" class="text-[0.88rem]">
+                          {{ skill.name }}
+                        </option>
+                      </select>
+                    </div>
+                  </div>
+                  <div>
+                    <label for="location" class="font-Satoshi500 !text-[1rem]">Location</label>
+                    <div
+                      class="bg-[#fff] w-full mt-[0.5rem] font-light font-Satoshi400 !p-4 border-gray-300 border-[0.509px] opacity-[0.8029] rounded-[6.828px] text-[0.88rem]"
+                    >
+                      <select
+                        v-model="country"
+                        :bordered="false"
+                        :show-arrow="false"
+                        class="w-full !outline-none !px-0 cursor-pointer text-[#000000] font-Satoshi500 leading-[1.75rem]"
+                        show-search
+                        @change="getCountryCode"
+                      >
+                        <option disabled value="Nigeria" class="text-[1rem] font-Satoshi500">Nigeria</option>
+                        <option v-for="country in contriesCode?.data" :key="country.id" :value="country.iso2" 
+                        class="text-[0.88rem]"
+                        >
+                          {{ country.name }}
+                        </option>
+                      </select>
+                    </div>
+                    <div
+                      class="bg-[#fff] w-full mt-[0.5rem] font-light font-Satoshi400 !p-4 border-gray-300 border-[0.509px] opacity-[0.8029] rounded-[6.828px] text-[0.88rem]"
+                    >
+                      <select
+                        v-model="sortInput.Location"
+                        :bordered="false"
+                        :show-arrow="false"
+                        class="w-full !outline-none !px-0 cursor-pointer text-[#000000] font-Satoshi500 leading-[1.75rem]"
+                        show-search
+                      >
+                        <option disabled value="Select State" class="text-[1rem] font-Satoshi500">Select State</option>
+                        <option v-for="state in states?.data" :key="state.id" :value="state.name" class="text-[0.88rem]">
+                          {{ state.name }}
+                        </option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div
+                  class="w-full font-Satoshi500 !p-0 !text-[1.13638rem] leading-[2.55681rem] text-[#000] !bg-transparent !pb-6 !border-b-2 border-[#666666]"
+                  >
+                      <select
+                          v-model="sortInput.experienceLevel"
+                          placeholder="Experience"
+                          :bordered="false"
+                          :show-arrow="false"
+                          class="w-full !outline-none !px-0 cursor-pointer !text-[#000000] font-Satoshi500 text-[1rem] leading-[1.75rem] !bg-transparent"
+                          show-search
+                          
+                      >
+                          <option disabled value="Experience">Experience</option>
+                          <option v-for="item in Experience" :key="item.name" :value="item.name">
+                          {{ item.name }} ({{ item.year }})
+                          </option>
+                      </select>
+                  </div>
+                  <div
+                  class="w-full font-Satoshi500 !p-0 !text-[1.13638rem] leading-[2.55681rem] text-[#000] !bg-transparent !pb-6 !border-b-2 border-[#666666]"
+                  >
+                      <select
+                          v-model="sortInput.jobType"
+                          placeholder="Employment Type"
+                          :bordered="false"
+                          :show-arrow="false"
+                          class="w-full !outline-none !px-0 cursor-pointer !text-[#000000] font-Satoshi500 text-[1rem] leading-[1.75rem] !bg-transparent"
+                          show-search
+                          
+                      >
+                          <option disabled value="Job Type">Job Type</option>
+                          <option v-for="item in CandidateType" :key="item" :value="item">
+                          {{ item }}
+                          </option>
+                      </select>
+                  </div>
+
+                  <button
+                    @click="toggleFilter"
+                      class="bg-[#31795A] text-white w-full text-center mx-auto p-4 py-4 justify-center rounded-full font-Satoshi500 text-[12.103px] items-center flex mt-[0.5rem]"
+                      :class="
+                        !isFilter
+                          ? 'bg-gray-300 cursor-not-allowed'
+                          : 'bg-[#31795A] btn-hover-2'
+                      "
+                    >
+                      Apply Filter
+                    </button>
+                </div>
+            </section>
+          </div>
         </template>
       </Tabs>
     </div>
